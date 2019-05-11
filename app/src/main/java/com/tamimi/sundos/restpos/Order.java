ce28548e87d5ad142c463faa1ee617d8081abc5c
user54 <Rawan>
2019-05-11 12:13:33 +0300

    rawan - (refund_ *)

--- app/src/main/java/com/tamimi/sundos/restpos/Order.java
+++ app/src/main/java/com/tamimi/sundos/restpos/Order.java
@@ -1638,7 +1638,7 @@ public class Order extends AppCompatActivity {
                 } else
                     taxValue = ((wantedItems.get(k).getTax()-discount) * wantedItems.get(k).getTax() / 100) / (1 + (wantedItems.get(k).getTax() / 100));
             }
-
+         Log.e("tax "," = "+taxValue);
             OrderTransactionsObj.add(new OrderTransactions(orderTypeFlag, 0, today, Settings.POS_number, Settings.store_number,
                     voucherNo, k, "" + wantedItems.get(k).getItemBarcode(), wantedItems.get(k).getMenuName(),
                     wantedItems.get(k).getSecondaryName(), wantedItems.get(k).getKitchenAlias(), wantedItems.get(k).getMenuCategory(),
