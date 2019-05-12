008315c30127533bcef435279a7ce3896b7ede29
sundos <sundos.tamimi20@gmail.com>
2019-05-12 14:16:36 +0300

    sundos - (Order) tax

--- app/src/main/java/com/tamimi/sundos/restpos/BackOffice/BackOfficeActivity.java
+++ app/src/main/java/com/tamimi/sundos/restpos/BackOffice/BackOfficeActivity.java
@@ -3302,7 +3302,6 @@ public class BackOfficeActivity extends AppCompatActivity {
                 returnsDiscountText.setText("" + allDiscountReturn);
                 netDiscountText.setText("" + netDiscount);
 
-
                 salesServiceText.setText("" + totalServiceSales);
                 returnsServiceText.setText("" + totalServiceReturn);
                 netServiceText.setText("" + netService);
